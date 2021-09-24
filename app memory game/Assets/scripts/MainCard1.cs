409ed4e509912f5ce8bd13c6f731391565c90dbe
Cavy Melissa Tryon <10881022@my.uvu.edu>
2021-09-24 11:49:57 -0600

    work on memory game

--- app memory game/Assets/scripts/MainCard1.cs
+++ app memory game/Assets/scripts/MainCard1.cs
@@ -4,32 +4,25 @@ using UnityEngine;
 
 public class MainCard1 : MonoBehaviour
 {
-      [SerializeField] private SceneController controller;
     [SerializeField] private GameObject Card_Back;
 
     public void OnMouseDown()
     {
-        if(Card_Back.activeSelf && controller.canReveal)
+        if (Card_Back.activeSelf)
         {
             Card_Back.SetActive(false);
-            controller.CardRevealed(this);
         }
     }
 
     private int _id;
-    public int id
+    public int id; 
     {
         get { return _id; }
     }
 
     public void ChangeSprite(int id, Sprite image)
     {
-        _id = id;
-        GetComponent<SpriteRenderer>().sprite = image; //This gets the sprite renderer component and changes the property of it's sprite!
-    }
-
-    public void Unreveal()
-    {
-        Card_Back.SetActive(true);
+        _id id; 
+        GetComponent<SpriteRenderer>().sprite = image;
     }
 }
